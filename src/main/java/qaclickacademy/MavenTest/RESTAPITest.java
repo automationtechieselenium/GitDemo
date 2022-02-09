c1c3a41d158e8aef27cb86a3401ed7d687c50307
automationtechieselenium <sanjitapatnaik224@gmail.com>
2022-02-09 13:59:14 +0530

    This is commit from Asian time zone

--- src/main/java/qaclickacademy/MavenTest/RESTAPITest.java
+++ src/main/java/qaclickacademy/MavenTest/RESTAPITest.java
@@ -6,8 +6,12 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println(" postJira");
+		System.out.println(" postJira1");
+		System.out.println(" postJira2");
+		System.out.println(" postJira3");
 	}
+	
+	
 	@Test
 	public void deleteTwitter()
 	{
